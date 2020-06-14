25735eb045096e150955674e17ec19c0811292f4
Monzer <wizmgbrown@gmail.com>
2020-06-14 10:38:35 +0200

    first edit for assm1

--- Router/dishRouter.js
+++ Router/dishRouter.js
@@ -4,3 +4,57 @@ const bodyParser = require('body-parser');
 const dishRouter = express.Router();
 
 dishRouter.route('/');
+
+
+//Routes
+//All
+dishRouter.all('deshes' ,(req ,res , next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain')
+    });
+//Route GET /deshes
+dishRouter.get('/deshes' , (req , res ,next)=>{
+    res.end('We will send all the deshes to you very soon!');
+    });
+
+//Route POST /deshes
+dishRouter.post('/deshes' , (req , res ,next)=>{
+    res.end(`well add : ${req.body.name} with detels ${req.body.description}`);
+    });
+
+    //Route PUT /deshes
+dishRouter.put('/deshes' , (req , res ,next)=>{
+    res.statusCode = 403;
+    res.end('Not Supported Here!');
+    });
+
+    //Route DELETE /deshes
+dishRouter.delete('/deshes' , (req , res ,next)=>{
+    res.end('Not Allowd!');
+    });
+
+    //------------------------------------------------------------------
+
+    //Route GET /deshes/:deshid
+dishRouter.get('/deshes/:deshid' , (req , res ,next)=>{
+    res.end('We will send deshes '+ req.params.deshid + ' to you very soon!');
+    });
+
+//Route POST /deshes/:deshid
+dishRouter.post('/deshes/:deshid' , (req , res ,next)=>{
+    res.statusCode = 403;
+    res.end('Not Supported Here!');
+    });
+
+    //Route PUT /deshes/:deshid
+dishRouter.put('/deshes/:deshid' , (req , res ,next)=>{
+    res.write(`well update the desh:` + req.params.deshid + "\n");
+    res.end(`well update the desh:` + req.body.name + ` with ` + req.body.description);
+    });
+
+    //Route DELETE /deshes/:deshid
+dishRouter.delete('/deshes/:deshid' , (req , res ,next)=>{
+    res.end(`deleting ${req.params.deshid}`);
+    });
+
+    module.exports = dishRouter;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const morgan  = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./Router/dishRouter');
 
 const app = express();
 
@@ -11,58 +12,11 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
+app.use('/' , dishRouter);
 
-//Routes
-//All
-app.all('deshes' ,(req ,res , next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain')
-    });
 
-//Route GET /deshes
-app.get('/deshes' , (req , res ,next)=>{
-    res.end('We will send all the deshes to you very soon!');
-    });
 
-//Route POST /deshes
-app.post('/deshes' , (req , res ,next)=>{
-    res.end(`well add : ${req.body.name} with detels ${req.body.description}`);
-    });
 
-    //Route PUT /deshes
-app.put('/deshes' , (req , res ,next)=>{
-    res.statusCode = 403;
-    res.end('Not Supported Here!');
-    });
-
-    //Route DELETE /deshes
-app.delete('/deshes' , (req , res ,next)=>{
-    res.end('Not Allowd!');
-    });
-
-//------------------------------------------------------------------
-
-    //Route GET /deshes/:deshid
-app.get('/deshes/:deshid' , (req , res ,next)=>{
-    res.end('We will send deshes '+ req.params.deshid + ' to you very soon!');
-    });
-
-//Route POST /deshes/:deshid
-app.post('/deshes/:deshid' , (req , res ,next)=>{
-    res.statusCode = 403;
-    res.end('Not Supported Here!');
-    });
-
-    //Route PUT /deshes/:deshid
-app.put('/deshes/:deshid' , (req , res ,next)=>{
-    res.write(`well update the desh: ${req.params.deshid}`);
-    res.end(`well update the desh: ${req.params.name} with ${req.params.description}`);
-    });
-
-    //Route DELETE /deshes/:deshid
-app.delete('/deshes/:deshid' , (req , res ,next)=>{
-    res.end(`deleting ${req.params.deshid}`);
-    });
 
     //Server
 const Server = http.createServer(app);
